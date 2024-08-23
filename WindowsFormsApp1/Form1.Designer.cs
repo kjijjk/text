a6f5191fadccd05c4228383be60863a479625c75
rnjsw <rnjsw@DESKTOP-U6HI6TQ>
2024-08-23 15:58:55 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.파일FToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.열기OToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장SToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.종료XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일FToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일FToolStripMenuItem
+            // 
+            this.파일FToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일FToolStripMenuItem1,
+            this.toolStripSeparator2,
+            this.열기OToolStripMenuItem,
+            this.저장SToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.종료XToolStripMenuItem});
+            this.파일FToolStripMenuItem.Name = "파일FToolStripMenuItem";
+            this.파일FToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.파일FToolStripMenuItem.Text = "파일(F)";
+            // 
+            // 파일FToolStripMenuItem1
+            // 
+            this.파일FToolStripMenuItem1.Name = "파일FToolStripMenuItem1";
+            this.파일FToolStripMenuItem1.Size = new System.Drawing.Size(131, 22);
+            this.파일FToolStripMenuItem1.Text = "새 파일(N)";
+            this.파일FToolStripMenuItem1.Click += new System.EventHandler(this.새파일ToolStripMenuItem1_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(128, 6);
+            // 
+            // 열기OToolStripMenuItem
+            // 
+            this.열기OToolStripMenuItem.Name = "열기OToolStripMenuItem";
+            this.열기OToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.열기OToolStripMenuItem.Text = "열기(O)";
+            this.열기OToolStripMenuItem.Click += new System.EventHandler(this.열기OToolStripMenuItem_Click);
+            // 
+            // 저장SToolStripMenuItem
+            // 
+            this.저장SToolStripMenuItem.Name = "저장SToolStripMenuItem";
+            this.저장SToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.저장SToolStripMenuItem.Text = "저장(S)";
+            this.저장SToolStripMenuItem.Click += new System.EventHandler(this.저장SToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(128, 6);
+            // 
+            // 종료XToolStripMenuItem
+            // 
+            this.종료XToolStripMenuItem.Name = "종료XToolStripMenuItem";
+            this.종료XToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.종료XToolStripMenuItem.Text = "종료(X)";
+            this.종료XToolStripMenuItem.Click += new System.EventHandler(this.종료XToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(800, 426);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "메모장_권준훈";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 파일FToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 열기OToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장SToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 종료XToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string currentFilePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void 새파일ToolStripMenuItem1_Click(object sender, EventArgs e)
+
+        {
+            if (CheckIfUnsavedChanges())
+            {
+                textBox1.Clear();
+                currentFilePath = null;
+            }
+        }
+
+        private void 열기OToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (CheckIfUnsavedChanges())
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        currentFilePath = openFileDialog.FileName;
+                        textBox1.Text = File.ReadAllText(currentFilePath);
+                    }
+                }
+            }
+        }
+
+        private void 저장SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void 종료XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (CheckIfUnsavedChanges())
+            {
+                Application.Exit();
+            }
+        }
+
+        private bool CheckIfUnsavedChanges()
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                var result = MessageBox.Show("저장하시겠습니까?", "저장", MessageBoxButtons.YesNoCancel);
+                if (result == DialogResult.Yes)
+                {
+                    SaveFile();
+                    return true;
+                }
+                else if (result == DialogResult.No)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void SaveFile()
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        currentFilePath = saveFileDialog.FileName;
+                        File.WriteAllText(currentFilePath, textBox1.Text);
+                    }
+                }
+            }
+            else
+            {
+                File.WriteAllText(currentFilePath, textBox1.Text);
+            }
+        }
+
+
+    }
+}
